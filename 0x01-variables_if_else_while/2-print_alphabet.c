d2f9e6a02fadba7f1df554ef099b830c1114293a
Edifice-GitHub <7thonwc@gmail.com>
2023-08-11 16:54:27 -0700

    Betty

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
 /**
-   * main - print alphabet in lowercase
-   *
-   *Return: zero
-   */
+ * main - print 00 to 99
+ *
+ * Return: zero
+ */
 int main(void)
 {
-	char ch = 'a';
+	int i;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch);
-		putchar('\n');
+	for (i = 0; i <= 99; i++)
+	{
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+		if (i == 99)
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
 	return (0);
 }

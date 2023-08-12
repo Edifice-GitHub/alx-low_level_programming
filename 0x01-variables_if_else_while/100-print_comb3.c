335f27883f6d10f7addb496e173031fd7eddf493
Edifice-GitHub <7thonwc@gmail.com>
2023-08-11 17:51:16 -0700

    optional

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
-
 /**
- * main - print 00 to 99
- *
- * Return: zero
+ * main - prints out all the numbers between 00 and 99
+ * with no two digits being the same
+ * Return: 0
  */
 int main(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 48; i < 58; i++)
 	{
-		putchar((i / 10) + '0');
-		putchar((i % 10) + '0');
-		if (i == 99)
-			break;
-		putchar(',');
-		putchar(' ');
+		for (j = i; j < 58; j++)
+		{
+			if (i == j)
+			{
+				continue;
+			}
+
+			putchar(i);
+			putchar(j);
+
+			if (i == 56 && j == 57)
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 	putchar('\n');
 	return (0);

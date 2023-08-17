f719decbd0dfb6ddb3722677965b6ab5ff48be92
Edifice-GitHub <7thonwc@gmail.com>
2023-08-17 14:49:49 -0700

    confused

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,13 +2,18 @@
 
 /**
  * print_alphabet_x10 - Make alphabet x10 times
- *
+  *Description - stdio not applicable
  * Return: void
  */
 void print_alphabet_x10(void)
+
 {
-	char c;
+
 	int i = 0;
+
+	for (i = 0; i <10; i++)
+
+		char c;
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
@@ -16,7 +21,6 @@ void print_alphabet_x10(void)
 		}
 
 		_putchar('\n')
-
-		i++;
+		
 	}
 }

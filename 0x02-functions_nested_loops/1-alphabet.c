cfa756891f35c79bead59b360b6cfb613531e26b
Edifice-GitHub <7thonwc@gmail.com>
2023-08-20 02:19:55 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_alphabet - Make the alphabet
+ * print_alphabet - a function that prints the alphabet, in lowercase
  *
- * Return: void
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
-	char c;
+	int n;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (n = 'a'; n <= 'z; c++)
 	{
-		_putchar(c)
+		_putchar(n);
 	}
 
 	_putchar('\n');
